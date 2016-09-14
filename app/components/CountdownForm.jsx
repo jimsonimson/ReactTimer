66ed2d1e53e099cde53629f00ffc90b0cce494e9
Mike Hawk <jonathanimson@gmail.com>
2016-09-14 13:24:53 -0700

    added timer

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -8,6 +8,8 @@ var CountdownForm = React.createClass({
     if (strSeconds.match(/^[0-9]*$/)) {
       this.refs.seconds.value = '';
       this.props.onSetCountdown(parseInt(strSeconds, 10))
+    } else {
+      alert("you need to use whole numbers only")
     }
   },
   render: function(){
